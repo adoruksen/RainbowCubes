6b23eb5e0b6afa5a18ae842b33a1e6990751cc3d
Doruk <73473236+adoruksen@users.noreply.github.com>
2022-11-05 01:18:22 +0300

    Initial commit

---
+++ Assets/Scripts/Player/LeftCube/LeftStacker.cs
@@ -0,0 +1,124 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using DG.Tweening;
+
+public class LeftStacker : MonoBehaviour
+{
+    [SerializeField] Canvas gameOverCanvas;
+    [SerializeField] Transform target;
+    [SerializeField] GameObject[] emojis = new GameObject[4];
+
+    public Stack<GameObject> stack; //LIFO
+
+    NavMeshAgent navMesh;
+    GameObject stackedCube;
+    AudioSource audioSource;
+    Tween punchScaleTween;
+    Color cubeColor;
+    Renderer rend;
+    GameObject trail;
+    TrailRenderer trailRend;
+    Rigidbody rb;
+    PlayerBaseOffset OnT;
+
+    Vector3 punchScale = new Vector3(.3f, .3f, .3f);
+    Vector3 addPosEmoji = new Vector3(-1f, 6f, 0f);
+    Vector3 trailPos = new Vector3(0f, -.5f,0f);
+
+    float xPos, yPos, zPos;
+    float delayInSeconds = .5f;
+    int vibrato = 10;
+    float duration = 1f;
+    float elasticity = 1f;
+    float strenght = 90f;
+    float randomness = 90f;
+    int currentNumb = 0;
+    float yTrail = -.4f;
+
+    void Start()
+    {
+        DOTween.Init();
+
+        gameOverCanvas.enabled = false;
+
+        stack = new Stack<GameObject>();
+
+        navMesh = GetComponent<NavMeshAgent>();
+
+        audioSource = GetComponent<AudioSource>();
+        audioSource.Stop();
+
+        trail = GameObject.FindWithTag("TrailLeft");
+        trailRend = trail.GetComponent<TrailRenderer>();
+        trailRend.enabled = false;
+
+        rb = GetComponent<Rigidbody>();
+
+        OnT = GetComponentInParent<PlayerBaseOffset>();
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "StackableCube")
+        {
+            StackedCube(other);
+            navMesh.baseOffset++;
+        }
+
+        else if (other.gameObject.tag == "ObstacleCube" || other.gameObject.tag == "Stair")
+        {
+            OnT.OnTriggerEnter(other);
+        }
+    }
+
+    public void StackedCube(Collider other)
+    {
+        audioSource.Play();
+
+        stackedCube = other.gameObject;
+        stack.Push(stackedCube);
+
+        SetPosition();
+        stackedCube.transform.position = new Vector3(xPos, yPos, zPos);
+        stackedCube.transform.parent = gameObject.transform;
+        stackedCube.tag = "Untagged";
+
+        GetRandomEmoji();
+        PunchScaleCube(other);
+        SetTrailColor();
+    }
+
+    public void SetPosition()
+    {
+        xPos = transform.position.x;
+        yPos = transform.position.y;
+        zPos = transform.position.z;
+        yPos -= stack.Count;
+
+        trail.transform.position = new Vector3(xPos, yTrail, zPos);
+    }
+
+    void SetTrailColor()
+    {
+        trailRend.enabled = true;
+        rend = trailRend.GetComponent<Renderer>();
+        cubeColor = stackedCube.GetComponent<Renderer>().material.color;
+        rend.material.color = cubeColor;
+    }
+
+    void GetRandomEmoji()
+    {
+        int randomIndex = Random.Range(0, emojis.Length);
+        GameObject instantiatedEmoji = Instantiate(emojis[randomIndex], (transform.position + addPosEmoji), Quaternion.identity) as GameObject;
+        instantiatedEmoji.transform.LookAt(target);
+        instantiatedEmoji.transform.DOShakeRotation(duration, strenght, vibrato, randomness, true).OnComplete( () => { Destroy(instantiatedEmoji); } );
+    }
+
+    void PunchScaleCube(Collider other)
+    {
+        punchScaleTween.Complete();
+        punchScaleTween = other.gameObject.transform.DOPunchScale(punchScale, duration, vibrato, elasticity);
+    }
+}
---
+++ README.md
@@ -0,0 +1,26 @@
+# RainbowCubes
+
+- Unity version: LTS Release 2021.3.3f1
+
+ANCIENT PEACOCK ASSESSMENT FOR GAME DEVELOPER CANDIDATES
+
+	OBJECTIVE
+		- Your objective is to improve "RainbowCubes" game within a few days.
+		 
+	FOCUS ON
+		- Refactoring the scripts.
+		- Improving Input/Output performance in terms of frames per seconds, refresh rate, fluency, convenience etc.
+		- Making the project more scalable and readable.
+		- Adding new features up to you.
+		- Preparing the project playable in Unity Editor with no extra effort.
+		- Using SOLID principles and Design Patterns under Object Oriented Programming.
+		- Hyper casual game development techniques
+
+	NOTES
+		- When you open your case project first time, you may need Device Simulator or Unity Remote to test the game.
+		- You can add, remove and modify anything in the project.
+
+	SUBMISSION
+		- Clone or fork this repository and put it in your github profile. Don't forget to share your repository link with us.
+
+GOOD LUCK!!!
